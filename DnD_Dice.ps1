b38bf43dad75aad7f791c2f68427c4e7015835f6
David Willson <dwillson@peckham.org>
2024-11-07 12:20:08 -0500

    Fixed max number, validate set and updated comments on DnD Dice script.

--- DnD_Dice.ps1
+++ DnD_Dice.ps1
@@ -1,38 +1,36 @@
 <#
 .SYNOPSIS
-This script will roll a D and D die
-.PARAMETER
-The parameter is for which type of die will be cast: "d4","d6","d8","d12","d20".
+This sciprt emulates a Dungeons & Dragons dice being rolled.
 #>
 
 [CmdletBinding()]   
     param (
-        [Parameter(Mandatory=$true)]
-        [ValidateSet("d4","d6","d8","d12","d20")]
+        [Parameter(Mandatory)]
+        [ValidateSet("d4","d6","d8","d10","d12","d20")]
         $Die  
     )
 
 #After the die is rolled (die type entered) the script will use the correct if statement for the rolled die and Get-Random to generate the number rolled. 
 If ($Die -eq 'd4') {
-    $roll = Get-Random -Minimum 1 -Maximum 4
+    $roll = Get-Random -Minimum 1 -Maximum 5
     Write-Host "Your $Die rolled a $roll."}
 ElseIf  ($Die -eq 'd6') {
-    $roll = Get-Random -Minimum 1 -Maximum 6
+    $roll = Get-Random -Minimum 1 -Maximum 7
     Write-Host "Your $Die rolled a $roll."
 } 
 ElseIf  ($Die -eq 'd8') {
-    $roll = Get-Random -Minimum 1 -Maximum 8
+    $roll = Get-Random -Minimum 1 -Maximum 9
     Write-Host "Your $Die rolled a $roll."
 }   
 ElseIf  ($Die -eq 'd10') {
-    $roll = Get-Random -Minimum 1 -Maximum 10
+    $roll = Get-Random -Minimum 1 -Maximum 11
     Write-Host "Your $Die rolled a $roll."
 }
 ElseIf  ($Die -eq 'd12') {
-        $roll = Get-Random -Minimum 1 -Maximum 12
+        $roll = Get-Random -Minimum 1 -Maximum 13
         Write-Host "Your $Die rolled a $roll."
 }
 ElseIf ($Die -eq 'd20') {
-        $roll = Get-Random -Minimum 1 -Maximum 20
+        $roll = Get-Random -Minimum 1 -Maximum 21
         Write-Host "Your $Die rolled a $roll."
 }
