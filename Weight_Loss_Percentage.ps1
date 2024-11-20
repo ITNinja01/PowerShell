66b6865c4d3e0099b8a093aea17d50f3230d5e74
David Willson <dwillson@peckham.org>
2024-11-20 18:18:11 -0500

    Working on ReadHosts

--- Weight_Loss_Percentage.ps1
+++ Weight_Loss_Percentage.ps1
@@ -1,4 +1,4 @@
-Read-Host -Prompt "What "
-
+$Starting_Weight = Read-Host -Prompt "What is your starting weight?"
+$Current_Weight = Read-Host -Prompt "What is your current weight?"
 
 (($Current_Weight/$Starting_Weight)-1)*-1*100
