16dc90ce2ef6ece4b28444d39651e9fad654e1b3
David Willson <dwillson@peckham.org>
2024-11-20 09:32:00 -0500

    Added  a needed space.

--- Speech_Synthesizer_Example.ps1
+++ Speech_Synthesizer_Example.ps1
@@ -2,6 +2,7 @@
 .SYNOPSIS
 A quick example of how to use PowerShell with the built-in speech synthesizer in Windows.
 #>
+
 $Name = Read-Host -Prompt "What is your name?"
 $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $SpeechSynth.Speak("Hello, $Name")
