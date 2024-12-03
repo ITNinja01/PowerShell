081fcd53e0582f88bfc5e8ad487180850b390dae
IT Ninja <dwillson1@hotmail.com>
2024-12-03 06:51:36 -0500

    Added missing scripts from last merged. Happened due to files not being staged.

---
+++ API_Weather_Forecast.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+This script will show the weather forecast for now, next week of time and next 8 hours based off your public IP.
+.DESCRIPTION
+This script uses the public IP information from Ipinfo.io to feed the longitude and latitude to the Weather.Gov API which than receives the forecast.
+.COMPONENT
+Ipinfo.io, Weather.Gov API, PowerShell
+#>
+
+#Making a request to a public IP information service 
+$response = Invoke-RestMethod -Uri "http://ipinfo.io/json"
+
+#Extracts city, country, latitude and longitude from the response
+$location  = $response.loc -split ","
+$latitude  = $location[0]
+$longitude = $location[1]
+$City      = $response.city
+$Country   = $response.country
+Write-Host "$City, $Country Forecast"
+
+#Creating variables to access weather
+$API_Weather_URL = "https://api.weather.gov/points/$latitude,$longitude"
+$Full_Weather = Invoke-RestMethod $API_Weather_URL
+
+Write-Host "Latest:"
+(Invoke-RestMethod ($Full_Weather.properties.forecast)).Properties.periods | Select-Object name, detailedForecast -First 1 | Out-Default
+
+Write-Host "The next week:"
+(Invoke-RestMethod ($Full_Weather.properties.forecast)).Properties.periods | Select-Object name, temperature, shortForecast, windSpeed | Out-Default
+
+Write-Host "The next 8 hours:"
+$Hourly_Weather = (Invoke-RestMethod ($Full_Weather.properties.forecastHourly)).Properties.periods | Select-Object startTime, endTime, temperature, probabilityOfPrecipitation
+$Hourly_Weather[0..7]
---
+++ Weight_Change_Percentage.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+This script will calculate how much percent of weight you have lost or gained.
+#>
+
+#Saving original host colors
+$Org_Foreground = (Get-Host).UI.RawUI.ForegroundColor
+$Org_Background = (Get-Host).UI.RawUI.BackgroundColor
+
+[Float]$Starting_Weight = Read-Host -Prompt "What is your starting weight?"
+[Float]$Current_Weight  = Read-Host -Prompt "What is your current weight?"
+
+Clear-Host
+
+if ( $Current_Weight -lt $Starting_Weight) {
+    $percent = [Math]::Round((($Current_Weight/$Starting_Weight)-1)*-1*100, 2)
+    Write-Host "You have lost $percent% of your body weight." -BackgroundColor Green -ForegroundColor White
+}
+else {
+    $percent = [Math]::Round((($Current_Weight/$Starting_Weight)-1)*100, 2)
+    Write-Host "You have gained $percent% of your body weight." -BackgroundColor Red -ForegroundColor Yellow
+}
+
+#After changing foreground and background colors resetting back original host colors since PowerShell does hot do this on its own.
+(Get-Host).UI.RawUI.ForegroundColor = $Org_Foreground
+(Get-Host).UI.RawUI.BackgroundColor = $Org_Background
