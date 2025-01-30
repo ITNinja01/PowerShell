f79ab365656928910332945fe17d399ad026fa38
ITNinja01 <dwillson1@hotmail.com>
2025-01-30 14:52:08 -0500

    Finshed asking the user for input, getting the CPU input, and what to do when it is a tied game.

--- Rock_Paper_Scissors.ps1
+++ Rock_Paper_Scissors.ps1
@@ -1,13 +1,13 @@
 $User_Input = Read-Host -Prompt "Please choose the first letter for (R)ock, (P)aper or (S)cissors?"
 
 if ($User_Input -eq 'r') {
-    $User_Choice -eq 'rock'
+    $User_Choice = 'rock'
 }
 elseif ($User_Input -eq 'p') {
-    $User_Choice -eq 'paper' 
+    $User_Choice = 'paper' 
 }
 elseif ($User_Input -eq 's') {
-    $User_Choice -eq 'scissors'
+    $User_Choice = 'scissors'
 }
 else {
     Write-Host 'Please only choose the first letter for (R)ock, (P)aper or (S)cissors.'
@@ -16,5 +16,5 @@ else {
 $CPU_Choice = Get-Random 'rock', 'paper', 'scissors'
 
 if ($User_Choice -eq $CPU_Choice) {
-    Write-Host "There is a tie of $User_Choice and $CPU_Choice" 
+    Write-Host "There is a tie between $User_Choice and $CPU_Choice!" 
 } 
