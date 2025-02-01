a4a5757ad64f83c6250b6fbd670866649a0f1d59
ITNinja01 <dwillson1@hotmail.com>
2025-02-01 13:05:37 -0500

    Finished the logic for the game. Now must add information on why a player wins a round.

--- Rock_Paper_Scissors.ps1
+++ Rock_Paper_Scissors.ps1
@@ -18,3 +18,23 @@ $CPU_Choice = Get-Random 'rock', 'paper', 'scissors'
 if ($User_Choice -eq $CPU_Choice) {
     Write-Host "There is a tie between $User_Choice and $CPU_Choice!" 
 } 
+
+if ($User_Choice -eq 'rock' -and $CPU_Choice -eq 'scissors') {
+    Write-Host 'User Wins'
+}
+elseif ($User_Input -eq 'paper' -and $CPU_Choice -eq 'rock') {
+    Write-Host 'User Wins'
+}
+elseif ($User_Choice -eq 'scissors' -and $CPU_Choice -eq 'paper') {
+    Write-Host 'User Wins'
+}
+
+if ($User_Input -eq 'scissors' -and $CPU_Choice -eq 'rock') {
+    Write-Host 'CPU Wins'
+}
+elseif ($User_Choice -eq 'rock' -and $CPU_Choice -eq 'paper') {
+    Write-Host 'CPU Wins'
+}
+elseif ($User_Choice -eq 'paper' -and $CPU_Choice -eq 'scissors') {
+    Write-Host 'CPU Wins'
+}
