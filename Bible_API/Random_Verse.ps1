a89ae0a2b16e6d1592acd98884b7098f1cd0f8f5
IT Ninja <dwillson1@hotmail.com>
2025-04-04 20:55:37 -0400

    Using Select-Object for only displaying wanted content in random.

--- Bible_API/Random_Verse.ps1
+++ Bible_API/Random_Verse.ps1
@@ -1,4 +1,3 @@
 $bible_request = Invoke-RestMethod -Uri https://bible-api.com/data/web/random
 
-$bible_request.random_verse | Where-Object {-notmatch book_id}
-
+$bible_request.random_verse | select book, text | fl
