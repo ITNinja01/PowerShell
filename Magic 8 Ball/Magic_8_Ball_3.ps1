dff813233100580ffbfd1549c61c031db824de1c
ITNinja01 <dwillson1@hotmail.com>
2025-05-26 21:31:11 -0400

    Working on using Yaml for the Magic 8 Ball script.

---
+++ Magic 8 Ball/Magic_8_Ball_3.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.SYNOPSIS
+This script is as a text-based Magic 8 Ball using an answer file. I created the Magic Ball this way first.
+#>
+
+#Asking the user for a question, any input is fine and will not be used in the program. 
+$Question = Read-Host "Ask the Magic 8 Ball a Question"
+
+
+
+$Answers = ConvertFrom-Yaml (get-content "$PSScriptRoot\Magic_8_Ball_Answers.yaml" -Raw) 
+Get-Random -InputObject $Answers.Values
+
+#Carriage return
+$crlf = "`r`n"
+
+#Writes the answer to the screen
+Write-Host $crlf $The_Answer $crlf
