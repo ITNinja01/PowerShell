f21c47d0140a4a6a9edc87b867e6a7f5ed430536
IT Ninja <dwillson1@hotmail.com>
2025-06-04 20:41:38 -0400

    Finished the Open Numismat module. As I work with the JSON data and functions there maybe moreCMDLets to come.

--- OpenNumismat/Functions/Get-CountryByName.ps1
+++ OpenNumismat/Functions/Get-CountryByName.ps1
@@ -1,16 +1,21 @@
-function Get-CountryByName {
     <#
-    .SYNOPSIS
-    This function retrieves coins from a JSON file based on the country's name.
-    .DESCRIPTION
-    The Get-CountryByName function reads a JSON file containing coin data and filters the coins based on the specified country name.
-    .PARAMETER Name
-    The name of the country to filter the coins.
-    .EXAMPLE
-    Get-CountryByName -name "United States"
-    This command retrieves coins from the specified JSON file based on the country's name.
-    #>
+.SYNOPSIS
+This function retrieves coins from a JSON file based on the country's name.
+.DESCRIPTION
+The Get-CountryByName function reads a JSON file containing coin data and filters the coins based on the specified country name.
+.COMPONENT
+OpenNumismat
+.PARAMETER Name
+The name of the country to filter the coins.
+.EXAMPLE
+Get-CountryByName -name "United States"
+.NOTES
+Developer: ITNinja01
+Date: 06-04-2025   
+Version: 1.0.1
+#>
 
+function Get-CountryByName {
 [CmdletBinding()]
 param(
 [Parameter(Mandatory)]
