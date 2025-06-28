f5e896301a23ae2685084572baff005b5d79adf7
IT Ninja <dwillson1@hotmail.com>
2025-06-28 08:22:48 -0400

    Tested with Windows and checks that the PowerShell YAML module is here, if not it installs it.

--- Duke Nukem Quotes/Random_Duke_Nukem1.ps1
+++ Duke Nukem Quotes/Random_Duke_Nukem1.ps1
@@ -13,11 +13,23 @@ A spoken quote or printed quote in the console.
 PS> .\Random_Duke_Nukem1.ps1
 #>
 
+#Ensure the required module is installed
+$moduleName = 'powershell-yaml'
+if (-not (Get-Module -ListAvailable -Name $moduleName)) {
+    try {
+        Install-Module -Name $moduleName -Scope CurrentUser -Force -ErrorAction Stop -Verbose
+    } catch {
+        Write-Error "Could not install module '$moduleName'. Please install it manually."
+        exit 1
+    }
+}
+Import-Module $moduleName
+
 $Quotes =  ConvertFrom-Yaml (get-content "$PSScriptRoot\Quotes.yaml" -Raw)
 $The_Quote = Get-Random -InputObject $Quotes
 
 if ($IsWindows -eq $true ){
-   $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
     $SpeechSynth.Speak($The_Quote)
 } 
 else {
