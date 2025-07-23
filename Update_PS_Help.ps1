c1b3f1c181c813c2407e497886f970ac51c0c065
ITNinja01 <dwillson1@hotmail.com>
2025-07-23 10:50:54 -0400

    Started work on script to how what helps files are updated.

---
+++ Update_PS_Help.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+Updates PowerShell help files for the current user.
+.DESCRIPTION
+I wanted to know what help files were updated and when. This script updates the help files for the current user and checks the last modified date of the help files in the local and OneDrive paths.
+.
+#>
+Update-help -UICulture ENG   -Verbose -Force
+
+$Write_Info = {
+    Write-Host "These help files updated successfully." -ForegroundColor Green
+}
+
+if ($IsWindows -eq $true) {
+    #Paths where help files are stored
+$Local_Help_Path = "$env:USERPROFILE\Documents\PowerShell\Help"
+$OneDrive_Help_Path = "$env:OneDrive\Documents\PowerShell\Help"
+
+if (Test-Path $Local_Help_Path) {
+    Get-ChildItem $Local_Help_Path -Recurse | Where-Object {$_.LastWriteTime -ge ((get-date).AddDays(-1))}
+    & $Write_Info
+}
+if (Test-Path $OneDrive_Help_Path) {
+    Get-ChildItem $OneDrive_Help_Path -Recurse | Where-Object {$_.LastWriteTime -ge ((get-date).AddDays(-1))}
+    & $Write_Info
+}
+}
