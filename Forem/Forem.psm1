61d1152be3d4bed7e67772c09fb4045b759224ba
IT Ninja <dwillson1@hotmail.com>
2025-11-29 21:38:13 -0500

    All functions are done. Working on psd1 and psm1 files.

---
+++ Forem/Forem.psm1
@@ -0,0 +1,22 @@
+. $PSScriptRoot\Functions\Get-Core
+Export-ModuleMember -Function Get-Core
+. $PSScriptRoot\Functions\Get-DevTo.ps1
+Export-ModuleMember -Function Get-DevTo
+
+Get-Core.ps1
+Get-Crypto.ps1
+Get-Design.ps1
+Get-DevTo.ps1
+Get-DumbDevTo.ps1
+Get-Forem.ps1
+Get-Gamers.ps1
+Get-Golf.ps1
+Get-HMPL-JS .ps1
+Get-Maker.ps1
+Get-Music.ps1
+Get-OpenForem.ps1
+Get-Parenting.ps1
+Get-Popcorn.ps1
+Get-Scale.ps1
+Get-Security.ps1
+Get-VibeCoding.ps1
