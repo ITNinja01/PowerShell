e30cd78f2649986dba7331672e575fb50c5fc76d
IT Ninja <dwillson1@hotmail.com>
2025-12-13 09:30:54 -0500

    Finished copy module script... need to test.

--- Copy_Module_Testing.ps1
+++ Copy_Module_Testing.ps1
@@ -1,17 +1,31 @@
+$ModulePath = Read-Host -Prompt "Enter the full path of the module you want to copy"
+
+if (-not (Test-Path $ModulePath)) {
+    Write-Error "$ModulePath does not exist. Please provide a valid path."
+    exit 1
+}
+
+$ImportResult = {
+    Import-Module -Name $ModulePath -Force -Verbose
+}
+
 if ($IsWindows -eq $true) {
     #Paths where module files are stored
-    $Local_Module_Path = "$env:USERPROFILE\Documents\PowerShell\Modules"
+    $LocalModulePath = "$env:USERPROFILE\Documents\PowerShell\Modules"
     $OneDrive_Module_Path = "$env:OneDrive\Documents\PowerShell\Modules"
-
-    if (Test-Path $Local_Module_Path) {
-     
+    if (Test-Path $LocalModulePath) {
+        Copy-Item -Path $ModulePath -Destination $LocalModulePath -Recurse -Force -Verbose
+        & $ImportResult 
     }
-    if (Test-Path $OneDrive_Module_Pat) {
-       
-}
+    if (Test-Path $OneDrive_Module_Path) {
+        & Copy-Item -Path $ModulePath -Destination $OneDrive_Module_Path -Recurse -Force -Verbose
+        $ImportResult 
+    }
 }
 
 else {
     #Paths where help files are stored in Linux or Mac.
-    $Local_Module_Path = "/home/$env:USER/.local/share/powershell/Modules"
+    $LocalModulePath = "/home/$env:USER/.local/share/powershell/Modules"
+    Copy-Item -Path $ModulePath -Destination $LocalModulePath -Recurse -Force -Verbose  
+    & $ImportResult 
 }
